5fe5e4db15a966aeb6d3d1c058e03ed07f8dd0ba
mariosmello <maCAT1324>
2016-09-24 17:22:16 -0300

    Testing Eloquent

---
+++ tests/integration/models/ArticleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Article;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ArticleTest extends TestCase
+{
+
+    use DatabaseTransactions;
+
+    /** @test */
+    function it_fetches_trending_article()
+    {
+
+        //Given
+        factory(Article::class, 2)->create();
+        factory(Article::class)->create(['reads' => 10]);
+        $mostPopular = factory(Article::class)->create(['reads' => 28]);
+
+        // When
+        $articles = Article::trending();
+
+        // Then
+        $this->assertEquals($mostPopular->id, $articles->first()->id);
+        $this->assertCount(3, $articles);
+    }
+
+}
